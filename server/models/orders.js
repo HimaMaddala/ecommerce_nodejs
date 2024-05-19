583e85514a7eb134ee586a9c40e37901626327b3
HimaMaddala <maddalahima@gmail.com>
2024-05-19 19:08:40 +0530

    nodejs

---
+++ server/models/orders.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const orderSchema = new mongoose.Schema({
+  product_id: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  img_url: {
+    type: String,
+    required: true
+  },
+  size:{
+    type: String,
+    required: true
+  },
+  quantity:{
+    type:Number,
+    required:true
+  },
+  price:{
+    type:Number,
+    required:true
+  },
+  total_price:{
+    type:Number,
+    required:true
+  },
+  merchant:{type: String,
+  required: true
+}
+});
+  
+const OrdersModel = mongoose.model("orderstables", orderSchema);
+export default OrdersModel;
+  
